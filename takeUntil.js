fc1db75ede5e45bea3c00382c178de5917286ccc
Victoria Amirov <vika.amir20@gmail.com>
2023-05-29 17:28:20 -0700

    fineshed the takeUntil

---
+++ takeUntil.js
@@ -0,0 +1,39 @@
+// // helper function
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: [${arr1 }] === [${arr2}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${arr1}] !== [${arr2}]`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length != arr2.length) {
+    return false;
+  } 
+  for ( let i = 0; i < arr1.length; i++){
+    if(arr1[i] !== arr2[i]){
+      return false;
+    }
+  }
+  return true
+};
+
+
+const takeUntil = function(array, callback) {
+  const results = [];
+  for(let item of array){
+    if(callback(item)){
+      break;
+    }
+    results.push(item);
+  }
+  return results;
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1)
+
+
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ])
