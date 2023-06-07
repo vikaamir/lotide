760accc616f1e60959f31ab478f8bed981554102
Victoria Amirov <vika.amir20@gmail.com>
2023-06-07 11:25:40 -0700

    chai test for tail function

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,8 @@
-const assertEqual = require('../assertEqual');
-const tail = require('../tail');
+const assert = require('chai').assert;
+const tail   = require('../tail');
 
-
-const words0 =["Hello", "Lighthouse", "Labs"];
-// Test Case: Check the original array 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-assertEqual(tail(words0), ["Lighthouse", "Labs"]);
-assertEqual((words).length, 3);
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+});
