cdc5a28f8248bcf256ef7afb79eed4965b6f9232
Victoria Amirov <vika.amir20@gmail.com>
2023-06-07 09:46:47 -0700

    uptade function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,7 @@
+const eqArrays = require('./eqArrays'); 
+
+
+
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
     console.log(`✅✅✅ Assertion Passed: [${arr1 }] === [${arr2}]`);
@@ -6,17 +10,9 @@ const assertArraysEqual = function(arr1, arr2) {
   }
 };
 
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length != arr2.length) {
-    return false;
-  } 
-  for ( let i = 0; i < arr1.length; i++){
-    if(arr1[i] !== arr2[i]){
-      return false;
-    }
-  }
-  return true
-};
 
-assertArraysEqual()
+ const test= ["hey", "vika", "lhl"]
+ const test2 = ["hey", "vika", "lhl"]
+assertArraysEqual(test, test2);
 
+module.exports = assertArraysEqual;
