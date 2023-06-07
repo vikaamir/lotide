2a43c2175833147dd641c7a191309b81432ca890
Victoria Amirov <vika.amir20@gmail.com>
2023-06-07 09:47:45 -0700

    update

--- eqArrays.js
+++ eqArrays.js
@@ -1,27 +1,13 @@
+const assertEqual = require('./assertEqual');
 const eqArrays = function(arr1, arr2) {
-
-  if (arr1.length != arr2.length) {
+  if (arr1.length !== arr2.length) {
     return false;
-  } 
-  for ( let i = 0; i < arr1.length; i++){
-    if(arr1[i] !== arr2[i]){
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
-  return true
+  return true;
 };
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: [${actual }] === [${expected}]`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-
-};
-
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+module.exports = eqArrays;
