dfa8eacba770701fe62c9a795e546922fb15cca2
Victoria Amirov <vika.amir20@gmail.com>
2023-06-07 10:00:08 -0700

    uptade function

--- middle.js
+++ middle.js
@@ -1,24 +1,5 @@
-// helper functions 
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length != arr2.length) {
-    return false;
-  } 
-  for ( let i = 0; i < arr1.length; i++){
-    if(arr1[i] !== arr2[i]){
-      return false;
-    }
-  }
-  return true
-};
-
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Passed: [${arr1 }] === [${arr2}]`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: [${arr1}] !== [${arr2}]`);
-  }
-};
+const assertArraysEqual = require('./assertArraysEqual'); 
+const eqArrays = require('./eqArrays'); 
 
 // actual function
 const middle = function(arr){ //[1, 34, 98, 4, 5]    
@@ -33,12 +14,4 @@ const middle = function(arr){ //[1, 34, 98, 4, 5]
     return [arr[index - 1], arr[index]]
   }
 }
-
-
-// tests
-assertArraysEqual(middle([1]), [])
-assertArraysEqual(middle([1, 2]), [])
-assertArraysEqual(middle([1, 2, 3]), [2])
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3])
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3])
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
+module.exports = middle;
