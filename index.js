b81ff7a0b4f4f9893931148e547b175540f7e134
Victoria Amirov <vika.amir20@gmail.com>
2023-06-07 14:11:43 -0700

    finshed

---
+++ index.js
@@ -0,0 +1,30 @@
+// index.js
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const flatten = require('./flatten');
+const findKeyByValue = require('./findKeyByValue');
+const findKey = require('./findKey')
+const eqObjects = require ('./eqObjects')
+const countOnly = require('./countOnly')
+const countLetters = require('./countLetters')
+const takeUntil = require('./takeUntil')
+const map = require('./map')
+const letterPositions =require('./letterPositions')
+
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  flatten: flatten,
+  findKeyByValue: findKeyByValue,
+  findKey: findKey,
+  eqObjects: eqObjects,
+  countOnly: countOnly,
+  countLetters: countLetters,
+  takeUntil: takeUntil,
+  map: map,
+  letterPositions: letterPositions
+
+};
