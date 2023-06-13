5c50542b5e6e928254e6acef6ebded643e084bc1
Victoria Amirov <vika.amir20@gmail.com>
2023-06-13 14:27:24 -0700

    did dome changes

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,62 +1,15 @@
-// helper functions
-const eqObjects = function(object1, object2) {
-  // 1. creat if steatment that checkes if object1 = object2 and return true or false 
-  let shirtOne = Object.keys(object1)
-  let shirtTwo =  Object.keys(object2)
-  if (shirtOne.length !== shirtTwo.length){
-    return false
-  }
-
-  for (key of shirtOne){ // forof loop is looping throug keys
-    if(Array.isArray(object1[key])){
-      if(!(eqArrays(object1[key], object2[key]))){
-        return false
-      }
-    } else if (object1[key] !== object2[key]){
-      return false 
-    } 
-  }
-  return true
-};
-
-const eqArrays = function(arr1, arr2) {
-
-  if (arr1.length != arr2.length) {
-    return false;
-  } 
-  for ( let i = 0; i < arr1.length; i++){
-    if(arr1[i] !== arr2[i]){
-      return false;
-    }
-  }
-  return true
-};
-  
-
-
-
+const eqObjects = require('./eqObjects'); 
 
 //main function
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
-  if (eqObjects(actual, expected)) {
+  if (eqObjects(actual, expected)) {// check if actual is what expected
       console.log(`✅✅✅ Assertion Passed: [${inspect(actual)}] === [${inspect(expected)}]`);
-    } else {
+    } else { // if not 
       console.log(`🛑🛑🛑 Assertion Failed: [${inspect(actual)}] !== [${inspect(expected)}]`);
     }
 };
 
-  
-
-// test 
-const shirtObject = { color: "red", size: "medium" };
-const anotherShirtObject= { size: "medium", color: "red" };
-assertObjectsEqual(shirtObject, anotherShirtObject);
-
-
-const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
-const anotherMultiColorShirtObject = { size: "medium", colors: ["red","blue"] };
-assertObjectsEqual(multiColorShirtObject  , anotherMultiColorShirtObject)
-  
+assertObjectsEqual();
 
 module.exports = assertObjectsEqual;
