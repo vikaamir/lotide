cf3d06ba9b3e65c50c4444dd1da02db1bf639118
Victoria Amirov <vika.amir20@gmail.com>
2023-06-13 14:28:39 -0700

    fixed

--- countLetters.js
+++ countLetters.js
@@ -1,30 +1,16 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: [${actual }] === [${expected}]`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-
-};
+const assertEqual = require('./assertEqual'); 
 const countLetters = function(words){
   const  result1 ={}
-  for(letters of words){
-    if (letters !== " ") { 
-      if (result1[letters]) {
-        result1[letters] += 1;
+  for(letters of words){// loop the words
+    if (letters !== " ") { // avoid empty spaces 
+      if (result1[letters]) { 
+        result1[letters] += 1; // if is a letter +1
       } else {
-        result1[letters] = 1;
+        result1[letters] = 1; // else keep it 1
       }
     }
   }
   return result1
-} 
-console.log(countLetters("lighthouse in the house"));
-let testSen = countLetters("lighthouse in the house")
-
-assertEqual(testSen["h"], 4);
-assertEqual(testSen["e"], 3);
-assertEqual(testSen["u"], 2);
-assertEqual(testSen["t"], 2);
-
+};
 module.exports = countLetters;
+countLetters();
